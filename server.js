b1ba6c40413e3863a3340853e7f04846aa24046f
Ismael_Henrique <Ismael_Henrique@outlook.pt>
2024-05-14 16:58:42 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,64 @@
+import { fastify } from "fastify"
+import { DatabaseMemory } from "./database-memory.js"
+
+const server = fastify()
+
+const database = new DatabaseMemory()
+
+//Criando rotas
+
+server.post("/videos", (request, reply) => {
+
+  const { title, description, duration } = request.body
+
+  database.create({
+    title: title,
+    description: description,
+    duration: duration,
+  })
+
+  console.log(database.list())
+
+  return reply.status(201).send()
+})
+
+server.get("/videos", (request) => {
+
+  const search = request.query.search //Query params
+  
+  const videos = database.list(search)
+
+  return videos
+
+})
+
+server.put("/videos/:id", (request, reply) => {
+  
+  const videoId = request.params.id
+
+  const { title, description, duration } = request.body
+
+  database.update(videoId, {
+    title,
+    description,
+    duration
+  })
+  
+  return reply.status(204).send()
+  
+})
+
+server.delete("/videos", (request, reply) => {
+  
+  const videoId = request.params.id
+
+  database.delete(videoId)
+
+  return reply.status(204).send()
+
+})
+
+server.listen({
+  port: 3333,
+})
+
